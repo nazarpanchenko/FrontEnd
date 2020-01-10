0a2fe5294473e7071894a229a89e2a6db9802fc8
Nazar <panchenko.nazar.a@gmail.com>
2020-01-10 10:28:39 +0200

    edit

--- js/lesson1/index.js
+++ js/lesson1/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+
+const userName = 'John';
+const greeting = "I'm John";
+const profession = 'Developer';
+const $hobby = 'Walking';
+const FavouriteDessert = 'cake';
+let pet = 'dog';
+let superNumber;
+superNumber = 17;
