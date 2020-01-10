dbb6e4d8dc96623e46a6ecd8bbff1335d20fc8ed
Nazar <panchenko.nazar.a@gmail.com>
2020-01-10 13:07:24 +0200

    edit

--- js/lesson3/task2/index.js
+++ js/lesson3/task2/index.js
@@ -21,11 +21,11 @@ undefined
 '      '
 '   30d   '
 
-console.log("\'17d\' to number is " + Number('17d'));
+console.log("\'17\' to number is " + Number('17'));
 console.log("true to number is " + Number(true));
 console.log("false to number is " + Number(false));
 console.log("null to number is " + Number(null));
-console.log("true to number is " + Number(undefined));
+console.log("undefined to number is " + Number(undefined));
 console.log("\'   20   \' to number is " + Number('   20   '));
 console.log("\'      \' to number is " + Number('      '));
 console.log("\'   30d   \' to number is " + Number('   30d   '));
@@ -45,7 +45,7 @@ console.log('undefined to boolean is ' + Boolean(undefined));
 console.log('0 to boolean is ' + Boolean(0));
 console.log('-0 to boolean is ' + Boolean(-0));
 console.log('NaN to boolean is ' + Boolean(NaN));
-console.log('\"\" to boolean is ' + Boolean(''));
-console.log('\" \" to boolean is ' + Boolean(' '));
+console.log("\'\' to boolean is " + Boolean(''));
+console.log("\' \' to boolean is " + Boolean(' '));
 console.log('17 to boolean is ' + Boolean(17));
-console.log('\"Hello\" to boolean is ' + Boolean('Hello'));
+console.log("\'Hello\' to boolean is " + Boolean('Hello'));
