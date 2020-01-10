1ebfa8380d18409512986e5912a9add1499564a5
Nazar <panchenko.nazar.a@gmail.com>
2020-01-10 11:25:02 +0200

    edit

---
+++ js/lesson1/lesson2/task4/index.js
@@ -0,0 +1,9 @@
+let a = 4,
+    b = 2;
+
+console.log('a: ', a);
+console.log('a: ', a);
+console.log('a - b = ', a - b);
+console.log('a + b = ', a + b);
+console.log('a / b = ', a / b);
+console.log('a * b = ', a * b);
