faeab8f372181774e37004e7fa9e47c0651b35be
Nazar <panchenko.nazar.a@gmail.com>
2020-01-10 14:40:14 +0200

    edit

--- js/lesson3/task4/index.js
+++ js/lesson3/task4/index.js
@@ -14,8 +14,8 @@ false && true && false;
 true && true && true;
 
 console.log("false && true: ", false && true);
-console.log("false && true: ", false && true && false);
-console.log("false && true && true: ", false && true && true);
+console.log("false && true && false: ", false && true && false);
+console.log("true && true && true: ", true && true && true);
 
 
 /* Логические операторы могут применятся к любым типам данных */
@@ -28,11 +28,13 @@ undefined || null || 0 || '' || null;
 
 console.log("\'text\' || false: ", 'text' || false);
 console.log("true || \'text\': ", true || 'text');
-console.log("undefined || null || 0 || \'\' + null: ", undefined || null || 0 || '' || null);
+console.log("undefined || null || 0 || \'\' || null: ", undefined || null || 0 || '' || null);
 
 const customAmount = null; // не выводим
 const defaultAmount = 17; // не выводим
 const amount = customAmount || defaultAmount; // выводим значкние переменной amount в формате console.log('amount: ', amount);
+
+console.log('amount: ', amount);
 
 
 /* && находит первое ложное значение */
@@ -42,7 +44,7 @@ undefined && null && 0 && '' && null;
 
 console.log("\'text\' && false: ", 'text' && false);
 console.log("true && \'text\': ", true && 'text');
-console.log("undefined && null &&: \'\' && null", undefined && null && 0 && '' && null);
+console.log("undefined && null && 0 && \'\' && null", undefined && null && 0 && '' && null);
 
 
 /* оператор отрицания (OR) */
