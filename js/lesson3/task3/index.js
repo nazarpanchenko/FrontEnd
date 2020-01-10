997614c70fdf53753026687398735e655c42bf93
Nazar <panchenko.nazar.a@gmail.com>
2020-01-10 13:13:24 +0200

    edit

---
+++ js/lesson3/task3/index.js
@@ -0,0 +1,30 @@
+/* сравнение чисел */
+
+const a = 17;
+const b = 5;
+
+a > b;
+a <= b;
+a == b;
+a != b;
+
+console.log("a < b: ", a < b);
+console.log("a <= b: ", a <= b);
+console.log("a == b: ", a == b);
+console.log("a < b: ", a < b);
+
+/* сравнение строк */
+
+'a' < 'b';
+'ab' > 'a';
+
+console.log("\'a\' < \'b\': ", 'a' < 'b');
+console.log("\'ab\' > \'a\': ", 'a' > 'b');
+
+/* строгое сравнение */
+
+17 === 1;
+17 === 17;
+
+console.log("17 === 1: ", 17 === 1);
+console.log("17 === 17: ", 17 === 17);
