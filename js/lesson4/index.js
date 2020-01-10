f96fd22dd0cdaddbaffbb609e1ef3643c9c591bc
Nazar <panchenko.nazar.a@gmail.com>
2020-01-10 21:23:46 +0200

    edit

--- js/lesson4/index.js
+++ js/lesson4/index.js
@@ -1,5 +1,5 @@
 for (let i = 1; i <= 5; i++) {
-    console.log('\n=> ' + i + ' <=');
+    console.log('=> ' + i + ' <=');
 
     for (let j = 1; j <= 5; j++) {
         console.log(i + ' x ' + j + ' = ' + i * j);
