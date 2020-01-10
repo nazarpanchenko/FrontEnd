9829320b8c0663e47d70759d3fb95325d23caf0c
Nazar <panchenko.nazar.a@gmail.com>
2020-01-10 10:35:24 +0200

    edit

--- js/lesson1/lesson2/index.js
+++ js/lesson1/lesson2/index.js
@@ -1,6 +1,6 @@
 
 const userName = 'John';
-const greeting = "I'm John";
+const oneGreeting = "I'm John";
 const profession = 'Developer';
 const hobby = 'Walking';
 const favouriteDessert = 'cake';
