34f9322632b53ca07c7af0c0babc94964de48020
Nazar <panchenko.nazar.a@gmail.com>
2020-01-10 20:54:31 +0200

    edit

--- js/lesson4/task3/index.js
+++ js/lesson4/task3/index.js
@@ -1,8 +1,9 @@
 let m = 2,
     n = 10,
-    result = m;
+    result = 1;
 
 for (let i = m; i <= n; i++) {
-    if (i % 2) result += (m * n);
-    console.log('Result: ' + result);
+    if (i % 2)  result *= m;
 }
+
+console.log('Result: ' + result);
