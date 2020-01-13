9568acc8e41ccae2b516a937af05c6dce5d64f94
Nazar <panchenko.nazar.a@gmail.com>
2020-01-14 00:02:52 +0200

    edit

---
+++ js/lesson4/task5/index.js
@@ -0,0 +1,11 @@
+let m = 10,
+    n = 50,
+    result = 0;
+
+for (let i = m; i <= n; i++) {
+    if (i % 2 == 0 && i % 4 != 0) result += i;
+    if (i % 3 == 0) result -= i;
+    if (i % 4 == 0) result *= i;
+    if (i % 5 == 0) console.log(result);
+    if (i % 2 == 0 && i % 4 != 0 && i % 3 == 0 && i % 4 == 0 && i % 5 == 0 && i % 5 == 0) continue;
+}
