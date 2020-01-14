cfc78fb92e2852d0f940d0e3e78c368ebc0bd03b
Nazar <panchenko.nazar.a@gmail.com>
2020-01-14 10:02:32 +0200

    edit

---
+++ js/lesson4/task6/index.js
@@ -0,0 +1,13 @@
+let sum = 0;
+
+for (let i = 0; i <= 1000; i++) {
+    if (i % 2) {
+        sum += i;
+    }
+
+    if ((sum * 5) > 5000) {
+        console.log('Bigger');
+    } else {
+        console.log('Smaller or equal');
+    }
+}
