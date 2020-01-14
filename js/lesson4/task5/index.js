95320c468957c7bf03874c4e4a12acce8ba2cd0e
Nazar <panchenko.nazar.a@gmail.com>
2020-01-14 09:50:34 +0200

    edit

--- js/lesson4/task5/index.js
+++ js/lesson4/task5/index.js
@@ -3,20 +3,19 @@ let m = 10,
     result = 0;
 
 for (let i = m; i <= n; i++) {
-    if (i % 2 == 0 && i % 4 != 0) {
+    if (i % 5 == 0 || i % 2 == 0 && i % 4 != 0) {
         result += i;
     } 
-    else if (i % 3 == 0) {
+    else if (i % 5 == 0 || i % 3 == 0) {
         result -= i;
     }
-    else if (i % 4 == 0) {
+    else if (i % 5 == 0 || i % 4 == 0) {
         result *= i;
     }
+    // else if (i % 5 == 0) {
+    //     console.log(i);
+    // }
     else {
         continue;  
-    }
-
-    if (i % 5 == 0 && i % 2 == 0 || i % 4 != 0 || i % 3 == 0 || i % 4 == 0) {
-        console.log(i);
     }
 }
