bc6633f6117efaa9ddb29f7969923a690524f389
Nazar <panchenko.nazar.a@gmail.com>
2020-01-15 20:39:16 +0200

    edit

--- js/lesson4/task8/index.js
+++ js/lesson4/task8/index.js
@@ -4,7 +4,7 @@ for (let i = 1; i <= 1000; i++) {
     sum += i;
 }
 
-let quotient = sum / 1234,
+let quotient = Math.floor(sum / 1234),
     remainder = sum % 1234;
 
 quotient > remainder ? console.log('true') : console.log('false');
