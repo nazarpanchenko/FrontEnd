6e230b4ba9f0729855062082ee7729ec9a09732c
Nazar <panchenko.nazar.a@gmail.com>
2020-01-18 13:32:59 +0200

    edot

--- js/lesson5/task8/index.js
+++ js/lesson5/task8/index.js
@@ -1,1 +1,15 @@
-const square = num => num * num;
+function sumTo(from, to) {
+    let sum = 0;
+
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+function compareSums(a, b, c, d) {
+    return sumTo(a, b) > sumTo(c, d) ? true : false;
+}
+
+compareSums(4, 12, 15, 64);
