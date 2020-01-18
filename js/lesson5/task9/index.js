fddc600754b2ef37471dd401ab28fb6ff29414ce
Nazar <panchenko.nazar.a@gmail.com>
2020-01-18 13:38:13 +0200

    edit

--- js/lesson5/task9/index.js
+++ js/lesson5/task9/index.js
@@ -1,1 +1,11 @@
-const square = num => num * num;
+function findDivCount(a, b, n) {
+    let result = 0;
+
+    for (let i = a; i <= b; i++) {
+        if (i % n == 0) ++result;
+    }
+
+    return result;
+}
+
+findDivCount(1, 100);
