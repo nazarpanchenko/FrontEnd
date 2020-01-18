d6bf530b8de956639afc2b086d165bb858e20766
Nazar <panchenko.nazar.a@gmail.com>
2020-01-18 18:07:49 +0200

    edit

--- js/lesson6/task3/index.js
+++ js/lesson6/task3/index.js
@@ -1,5 +1,13 @@
-function sum (a, b) {
-    return a + b;
+function getSpecialNumbers(from, to) {
+    let specialNumbers = [];
+
+    for (let i = from; i <= to; i++) {
+        if (i % 3 == 0) {
+            specialNumbers.pop(i);
+        }
+    }
+
+    return specialNumbers;
 }
 
-getSum(2, 3);
+getSpecialNumbers(10, 30);
