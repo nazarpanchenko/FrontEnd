33354d5d296f9c98386d4e059a21ccf6ee35ea68
Nazar <panchenko.nazar.a@gmail.com>
2020-01-18 17:46:29 +0200

    edit

--- js/lesson6/task1/index.js
+++ js/lesson6/task1/index.js
@@ -1,5 +1,9 @@
-function getArrayBounds(arr) {
-    return [arr.length, arr[0], arr.length[-1]];
+function getArrayBound(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
+    return [arr.length, arr[0], arr[length - 1]];
 }
 
-getArrayBounds([1, 2, 3, 4, 5, 6]);
+const arrayBound = getArrayBound([1, 2, 3, 4, 5, 6]);
