e843061339286bb97466d64a899549ec7ed82d98
Nazar <panchenko.nazar.a@gmail.com>
2020-01-18 17:55:22 +0200

    edit

--- js/lesson6/task2/index.js
+++ js/lesson6/task2/index.js
@@ -1,5 +1,15 @@
-function sum (a, b) {
-    return a + b;
+let sum = 0;
+
+function getSum(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
 }
 
-getSum(2, 3);
+getSum([11, 22, 33, 55, 56]);
