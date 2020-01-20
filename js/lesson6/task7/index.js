1ac52231fd3d005f87b3e8ba354bc791100b0050
Nazar <panchenko.nazar.a@gmail.com>
2020-01-20 20:41:28 +0200

    edit

--- js/lesson6/task7/index.js
+++ js/lesson6/task7/index.js
@@ -1,5 +1,30 @@
-function sum (a, b) {
-    return a + b;
+function sortAsc(array) {
+    let sortedArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        sortedArr.push(arr[i]);
+    }
+
+    for (let j = 0, endJ = arr.length - 1; j < endJ; j++) {
+        let wasSwap = false;
+
+        for (let k = 0, endK = endK - k; j < endK; k++) {
+            if (arr[k] > arr[k + 1]) {
+                [sortedArr[k], sortedArr[k + 1]] = [sortedArr[k + 1], sortedArr[k]];
+                wasSwap = true;
+            }
+        }
+
+        if (!wasSwap) break;
+    }
+     
+    return sortedArr;
 }
 
-getSum(2, 3);
+function sortDesc(array) {
+    let sortedArr = [];
+
+
+}
+
+sortAsc();
