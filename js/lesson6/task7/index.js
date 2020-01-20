90a0126b238c265891fe4adc61e8ef42233f555f
Nazar <panchenko.nazar.a@gmail.com>
2020-01-20 21:15:06 +0200

    edit

--- js/lesson6/task7/index.js
+++ js/lesson6/task7/index.js
@@ -17,16 +17,32 @@ function sortAsc(arr) {
 
         if (!wasSwap) break;
     }
-
-    console.log(sortedArr);
      
     return sortedArr;
 }
 
-function sortDesc(array) {
+function sortDesc(arr) {
     let sortedArr = [];
 
+    for (let i = arr.length - 1; i < arr.length; i++) {
+        sortedArr.push(arr[i]);
+    }
 
+    for (let j = 0, endJ = arr.length - 1; j < endJ; j++) {
+        let wasSwap = false;
+
+        for (let k = 0, endK = endJ - j; k < endK; k++) {
+            if (sortedArr[k] > sortedArr[k + 1]) {
+                [sortedArr[k], sortedArr[k + 1]] = [sortedArr[k + 1], sortedArr[k]];
+                wasSwap = true;
+            }
+        }
+
+        if (!wasSwap) break;
+    }
+
+    return sortedArr.reverse();
 }
 
 sortAsc([6, 0, 13, 28, 14, 5, 82, 9]);
+sortDesc([6, 0, 13, 28, 14, 5, 82, 9]);
