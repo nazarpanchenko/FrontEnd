a13d3f0840e47c268ff1985a8b4f1ffa98dfccc9
Nazar <panchenko.nazar.a@gmail.com>
2020-01-21 20:09:15 +0200

    edit

--- js/lesson6/task8/index.js
+++ js/lesson6/task8/index.js
@@ -1,5 +1,13 @@
-function uniqueSum(array) {
-    return array.filter();
-}
+function uniqueSum(arr) {
+    let result = [];
+  
+    for (let num of arr) {
+      if (!result.includes(num)) {
+        result.push(num);
+      }
+    }
+  
+    return result;
+  }
 
 uniqueSum([3, 1, 13, 9, 4, 31, 23, 2]);
