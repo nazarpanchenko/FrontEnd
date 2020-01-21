44994e55079f606eaf58e9012a42fa2b20d0aa9e
Nazar <panchenko.nazar.a@gmail.com>
2020-01-21 20:58:34 +0200

    Edit

--- js/lesson6/task11/index.js
+++ js/lesson6/task11/index.js
@@ -1,5 +1,8 @@
-function sum (a, b) {
-    return a + b;
+function checker(arr) {
+    let min = Math.min.apply(null, arr),
+        max = Math.max.apply(null, arr);
+
+    return (min + max) > 1000 ? true : false;
 }
 
-getSum(2, 3);
+checker([3, 0, 9, 12, 53, 21, 16, 18]);
