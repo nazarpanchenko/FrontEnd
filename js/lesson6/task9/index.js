16a52eafe3fbc8779f57e97242e627f25dd938c2
Nazar <panchenko.nazar.a@gmail.com>
2020-01-21 20:23:07 +0200

    Edit

--- js/lesson6/task9/index.js
+++ js/lesson6/task9/index.js
@@ -1,5 +1,17 @@
-function sum (a, b) {
-    return a + b;
-}
+function removeDuplicates(array) {
+    let result = [];
 
-getSum(2, 3);
+    for (let i = 0; i < array.length; i++) {
+        result.push(array[i]);
+    }
+  
+    for (let number of result) {
+      if (result.includes(number)) {
+        result.pop(number);
+      }
+    }
+  
+    return result;
+  }
+
+removeDuplicates([3, 1, 13, 9, 4, 31, 23, 2]);
