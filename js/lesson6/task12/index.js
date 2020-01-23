f584bc909038789d680d7a25f379f8e83d38f4fb
Nazar <panchenko.nazar.a@gmail.com>
2020-01-23 16:10:50 +0200

    edit

--- js/lesson6/task12/index.js
+++ js/lesson6/task12/index.js
@@ -2,4 +2,4 @@ function withdraw(clients, balances, client, amount) {
     return amount > balances[i] ? -1 : balances[i] - amount;
 }
 
-withdraw(['Mark, Peter, Anthon, Carl'], [1250, 1000, 650, 880], 'Peter', 2000);
+withdraw(['Mark', 'Peter', 'Anthon', 'Carl'], [1250, 1000, 650, 880], 'Peter', 2000);
