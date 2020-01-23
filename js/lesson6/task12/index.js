520ed1cc54602f7ec5668966db07979dd7efcbdf
Nazar <panchenko.nazar.a@gmail.com>
2020-01-23 16:17:32 +0200

    Edit

--- js/lesson6/task12/index.js
+++ js/lesson6/task12/index.js
@@ -1,5 +1,11 @@
 function withdraw(clients, balances, client, amount) {
-    return amount > balances[i] ? -1 : balances[i] - amount;
+    let clientID;
+
+    for (let i = 0; i < clients.length; i++) {
+        if (clients[i] == client) clientID = i;
+    }
+
+    return amount > balances[clientID] ? -1 : balances[i] - amount;
 }
 
 withdraw(['Mark', 'Peter', 'Anthon', 'Carl'], [1250, 1000, 650, 880], 'Peter', 2000);
