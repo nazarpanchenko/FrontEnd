6a60bb4915047a3636093753669125c7424ebc53
Nazar <panchenko.nazar.a@gmail.com>
2020-01-27 20:25:59 +0200

    Edit

--- js/lesson7/task1/index.js
+++ js/lesson7/task1/index.js
@@ -1,11 +1,19 @@
+function checkIfSpecialNumber(num) {
+    if (num % 3 == 0) {
+        specialNumbers.push(num);
+    }
+}
+
 const getSpecialNumbers = numbers => {
     let specialNumbers = [];
 
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 3 === 0) {
-            specialNumbers,push(numbers[i]);
-        }
-    }
+   numbers.forEach(function checkIfSpecialNumber(num) {
+       if (num % 3 === 0) {
+           specialNumbers.push(num);
+       }
+   });
+
+    numbers.forEach(checkIfSpecialNumber());
 
     return specialNumbers;
 };
