d8bbf4788027c480db0f33520f0f1b1a3599bde0
Nazar <panchenko.nazar.a@gmail.com>
2020-01-27 23:34:08 +0200

    Edit

--- js/lesson7/task4/index.js
+++ js/lesson7/task4/index.js
@@ -1,6 +1,6 @@
 const getMessagesForBestStudents = (allStudents, failedStudents) => {
-    const passedStudents= allStudents
-        .filter(name => !(failedStudents.includes(name)));
+    const passedStudents = allStudents
+        .filter(name => !failedStudents.includes(name));
 
     return passedStudents
         .map(name => 'Good Job, ' + name);
