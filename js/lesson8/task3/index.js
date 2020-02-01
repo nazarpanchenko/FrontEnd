71439a3aaf419906efeb042f53046ea1f806572e
Nazar <panchenko.nazar.a@gmail.com>
2020-02-01 20:27:20 +0200

    edit

--- js/lesson8/task3/index.js
+++ js/lesson8/task3/index.js
@@ -1,15 +1,9 @@
 const pickProps = (obj, keys) => {
     let result = {};
 
-    for (key in obj) {
-        result[key] = key;
-    }
-
     for (let el of keys) {
-        if (result.hasOwnProperty(el)) result[el] = el;
+        if (obj.hasOwnProperty(el)) result[el] = el;
     }
-
-    console.log(result);
 
     return Object.keys(result).length == 0 ? {} : result;
 };
