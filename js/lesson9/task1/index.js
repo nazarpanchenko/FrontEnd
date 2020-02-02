82d74d2346a63070763cb071246a8e7f6a5925b9
Nazar <panchenko.nazar.a@gmail.com>
2020-02-02 20:22:03 +0200

    Edit

--- js/lesson9/task1/index.js
+++ js/lesson9/task1/index.js
@@ -1,17 +1,11 @@
-const getAdults = people => {
-    let result = [];
-
-    for (let person of people) {
-        if (people[person] >= 18) result.push(person);
-    }
-
-    return Object.keys(people).length == 0 ? {} : people;
-};
-
-const people = {
+const user = {
     'John Doe' : 19,
     'Tom' : 17,
     'Bob' : 18
 };
 
-getAdults(people);
+const getAdults = usersObj => result = Object.entries(people)
+    .filter(user => user[1] >= 18)
+    .map(user => user[0]);
+
+console.log(getAdults(usersObj));
