2a591bace43194b4c509b109c6ee0c2bb38f5320
Nazar <panchenko.nazar.a@gmail.com>
2020-02-02 20:34:27 +0200

    edit

--- js/lesson9/task2/index.js
+++ js/lesson9/task2/index.js
@@ -1,13 +1,1 @@
-function transformToObject(arr) {
-    if (arr.length == 0) return {};
-
-    let result = {};
-
-    arr.forEach(function(item, arr) {
-        result[item] = item;
-    });
-
-    return result;
-}
-
-transformToObject([1, 2, 3, 4, 5, 6, 'a', 'b']);
+const copyObj = obj => Object.assign({}, obj);
