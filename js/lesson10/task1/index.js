9ad99d31b4c3dd9d5a9e4b183d2461529ac3ac8b
Nazar <panchenko.nazar.a@gmail.com>
2020-02-10 21:20:57 +0200

    edot

--- js/lesson10/task1/index.js
+++ js/lesson10/task1/index.js
@@ -1,10 +1,57 @@
-const parseArray = arr => {
-    const result = arr
-        .map(el => Number.parseFloat(el));
+const getFiniteNumbers = arr => {
+    const result = [];
+
+    arr.forEach(el => {
+        result.push(Number.isFinite(el));
+    });
 
     return result;
 };
 
-const elementList = [4, 5, 6, '4.17  K', Infinity, undefined];
+const getFiniteNumbersV2 = arr => {
+    const result = [];
 
-console.log(parseArray(elementList));
+    arr.forEach(el => {
+        result.push(isFinite(el));
+    });
+
+    return result;
+};
+
+const getNaN = arr => {
+    const result = [];
+
+    arr.forEach(el => {
+        result.push(Number.isNaN(el));
+    });
+
+    return result;
+};
+
+const getNaNV2 = arr => {
+    const result = [];
+
+    arr.forEach(el => {
+       result.push(isNaN(el));
+    });
+
+    return result;
+};
+
+const getIntegers = arr => {
+    const result = [];
+
+    arr.forEach(el => {
+       result.push(Number.isInteger(el));
+    });
+
+    return result;
+};
+
+const arr = [2, 6, Infinity, NaN, null, undefined, 0];
+
+console.log(getFiniteNumbers(arr));
+console.log(getFiniteNumbersV2(arr));
+console.log(getNaN(arr));
+console.log(getNaNV2(arr));
+console.log(getIntegers(arr));
