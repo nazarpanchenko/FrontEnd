82c41d58300a690588126af304d4e47a57c4bd01
Nazar <panchenko.nazar.a@gmail.com>
2020-02-10 23:58:50 +0200

    edit

--- js/lesson10/task7/index.js
+++ js/lesson10/task7/index.js
@@ -6,6 +6,8 @@ const getRandomNumbers = (arrLength, min, max) => {
     for (let i = 0; i < arrLength; i++) {
         let rand = Math.round(min + Math.random() * (max - min));
 
+        if (!Number.isInteger(rand)) return null;
+
         result.push(rand);
     }
 
