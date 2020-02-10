16495555cc166450a047cb30da4107ea6a197807
Nazar <panchenko.nazar.a@gmail.com>
2020-02-10 22:18:14 +0200

    Edit

--- js/lesson10/task6/index.js
+++ js/lesson10/task6/index.js
@@ -2,10 +2,10 @@ const getTotalPrice = numbers => {
     let sum = 0;
 
     numbers.forEach(el => {
-        sum += Math.floor(el);
+        sum += Math.floor(el).toFixed(2);
     });
 
-    return '$' + sum.toFixed(2);
+    return '$' + sum;
 };
 
 const numbers = [6, 0, 3, 8, 12, 24, 11, 9];
