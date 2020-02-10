591e5bbecb4624bc1e9f815d7094129c62478488
Nazar <panchenko.nazar.a@gmail.com>
2020-02-10 19:45:54 +0200

    Edit

---
+++ js/lesson10/task3/index.js
@@ -0,0 +1,11 @@
+const multiRound = num => [
+    Math.round(num * 100) / 100,
+    Math.floor(num * 100) / 100,
+    Math.ceil(num * 100) / 100,
+    Math.trunc(num * 100) / 100,
+    num.toFixed(2)
+];
+
+
+
+console.log(multiRound(Math.PI));
