bf20c6e4b0de8a7d6590fb080f00bfe5618dfc04
Nazar <panchenko.nazar.a@gmail.com>
2020-02-10 19:31:15 +0200

    edit

---
+++ js/lesson10/task1/index.js
@@ -0,0 +1,8 @@
+const parseArray = arr => {
+    const result = arr
+        .map(el => Number.parseFloat(el));
+
+    return result;
+};
+
+const elementList = [4, 5, 6, '4.17  K', Infinity, undefined];
