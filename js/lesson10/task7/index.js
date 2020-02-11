e0a8f0f2fccfd231eed0c56205790aadce3fa31c
Nazar <panchenko.nazar.a@gmail.com>
2020-02-11 22:08:43 +0200

    Edit

--- js/lesson10/task7/index.js
+++ js/lesson10/task7/index.js
@@ -1,14 +1,13 @@
 const getRandomNumbers = (arrLength, from, to) => {
-    let result = [];
+    let result = [],
+        isWrongRange = to < from,
+        noNumbersBetween = (to - from < 1) && Math.ceil(to) === Math.ceil(from);
+
+    if (isWrongRange || noNumbersBetween) {
+        return null;
+    }
 
     for (let i = 0; i < arrLength; i++) {
-        let isWrongRange = to < from,
-            noNumbersBetween = (to - from < 1) && Math.ceil(to) === Math.ceil(from);
-
-        if (isWrongRange || noNumbersBetween) {
-            return null;
-        }
-
         let rand = from + Math.random() * (to - from);
 
         result.push(Math.floor(rand));
