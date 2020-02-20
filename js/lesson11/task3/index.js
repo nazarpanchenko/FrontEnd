f09efc988ea1a47131311fd7802da6826357ee74
Nazar <panchenko.nazar.a@gmail.com>
2020-02-21 00:41:13 +0200

    Edit

--- js/lesson11/task3/index.js
+++ js/lesson11/task3/index.js
@@ -1,7 +1,7 @@
 const splitString = (str, length) => {
     if (typeof(str) != 'string') return null;
 
-    if (!length) length = 10;
+    if (length == undefined) length = 10;
 
     let strArr = [];
     let startPos = 0;
