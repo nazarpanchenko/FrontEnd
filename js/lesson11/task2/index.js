0ec0cc3ffefbd56794a8426529754c1f655a57d4
Nazar <panchenko.nazar.a@gmail.com>
2020-02-20 23:18:16 +0200

    edit

--- js/lesson11/task2/index.js
+++ js/lesson11/task2/index.js
@@ -11,4 +11,11 @@ const sortContacts = (contacts, direction) => {
     return result;
 };
 
-sortContacts({ name: "Tom", phoneNumber: '777-77-77' }, true);
+const contacts = [
+    { name: "Tom", phoneNumber: '777-77-77' },
+    { name: "John", phoneNumber: '666-77-77' },
+    { name: "Jack", phoneNumber: '555-77-77' },
+    { name: "Bob", phoneNumber: '444-77-77' }
+];
+
+sortContacts(contacts, true);
